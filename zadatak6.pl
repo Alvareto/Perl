61e54ab86cb1eefe909fd9b40fe8fd95d5495b92
Ivan Grgurina <ivan.grgurina@gmail.com>
2014-05-16 10:52:16 +0200

    Fixed bugs
    String manipulation didn't took empty strings into consideration.

--- zadatak6.pl
+++ zadatak6.pl
@@ -1,25 +1,22 @@
-Napisati Perl skriptu koja 
-ce u datoteci koja se zadaje
-kao argument naredbenog retka 
-izbrojati rijeci sa zajednickim
- preﬁksom. Duljina preﬁksa zadaje
-  se kao zadnji argument naredbenog
-   retka, a prethode mu imena datoteka
-    (može ih biti i više).
-
-use open ':locale';
-use locale;
+# Napisati Perl skriptu koja ce u datoteci koja se zadaje
+# kao argument naredbenog retka izbrojati rijeci sa zajednickim
+# preﬁksom. Duljina preﬁksa zadaje se kao zadnji argument naredbenog
+# retka, a prethode mu imena datoteka (može ih biti i više).
 
 my %count;
 
 $duljinaPrefiksa =  pop @ARGV;
 
 while (<>) {
-	foreach $word (split(/ /)) {
-		$count{lc substr($word, 0, $duljinaPrefiksa)}++;
+	#chomp $_;
+	foreach $word ($_ =~ /\w+/g) { #  split(/\s+/)
+		$_ = lc substr($word, 0, $duljinaPrefiksa);
+		if (tr/a-z/a-z/ == $duljinaPrefiksa) { #transliteracija - broji mala slova u $_
+			$count{$_}++;
+		}
 	}
 }
 
 for my $key ( sort keys %count ) {
-	print "$key : $count{$key}\n";
+	printf "%-6s : %s\n", $key, $count{$key};
 }
